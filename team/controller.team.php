f56ff8ccfa9030b0ead32f61d771b3e69b031432
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-07-23 17:36:48 +0300

    add teams and get teams

--- team/controller.team.php
+++ team/controller.team.php
@@ -1,18 +1,50 @@
 <?php
-session_start();
+require_once '../helper/database.php';
 class Team
 {
     public function getTeams()
     {
-        if (!isset($_SESSION["teams"])) {
-            DB::Init();
-            $sql = "SELECT * FROM team";
-            $result = DB::get()->query($sql);
-            $teams = $result->fetch_all(MYSQLI_ASSOC);
-            return $teams;
-        } else {
-            return $_SESSION["teams"];
+
+        DB::Init();
+        $sql = "SELECT * FROM team";
+        $result = DB::get()->query($sql);
+        $teams = $result->fetch_all(MYSQLI_ASSOC);
+        if ($teams == null) {
+            echo "No teams";
         }
 
+        return $teams;
+
     }
+    public function AddTeam()
+    {
+        DB::Init();
+        $sql = "INSERT INTO team (team)  VALUES (?)";
+        $stmt = DB::get()->stmt_init();
+
+        if (!$stmt->prepare($sql)) {
+            die("SQL error: " . DB::get()->error);
+        }
+
+        $stmt->bind_param("s", $_POST["team_name"]);
+
+        if (!$stmt->execute()) {
+            if (DB::get()->errno === 1062) {
+                echo "Same team exists";
+            } else {
+                die("SQL error: " . $stmt->error . " Error number: " . DB::get()->errno);
+            }
+        } else {
+
+            header("Location: view.team.php
+            ");
+            exit;
+        }
+    }
+}
+
+$team = new Team();
+$teams = $team->getTeams();
+if (isset($_POST["team_name"])) {
+    $team->AddTeam();
 }
--- team/view.team.php
+++ team/view.team.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+session_start();
 require_once 'controller.team.php';
 ?>
 
@@ -6,13 +9,39 @@ require_once 'controller.team.php';
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Teams Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
 </head>
 
 <body>
+    <h1>Teams Page</h1>
+    <form action="controller.team.php" method="post">
+        <label for="name">Name</label>
+        <input type="text" name="team_name" id="team_name">
+        <button>Add Team</button>
+    </form>
 
+
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($teams as $teams): ?>
+            <tr>
+                <td><?php echo $teams["id"] ?></td>
+                <td><?php echo $teams["team"]; ?></td>
+            </tr>
+            <?php endforeach;?>
+        </tbody>
+    </table>
 </body>
 
 </html>
